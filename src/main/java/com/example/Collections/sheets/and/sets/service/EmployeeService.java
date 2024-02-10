3801e2c11761c1e70ed786b6a745ff77233d9839
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-02-10 15:45:41 +0300

    TaskMap

--- src/main/java/com/example/Collections/sheets/and/sets/service/EmployeeService.java
+++ src/main/java/com/example/Collections/sheets/and/sets/service/EmployeeService.java
@@ -7,63 +7,66 @@ import com.example.Collections.sheets.and.sets.exception.IllegalArgumentExceptio
 import com.example.Collections.sheets.and.sets.model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService {
     private static final int employeeCount = 5;
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     public Employee createEmployee(String firstName, String lastName) {
         if (employees.size() >= employeeCount) {
             throw new EmployeeStorageIsFullException();
         }
-        Employee employee = new Employee(firstName, lastName);
+        String key = getKey(firstName, lastName);
         if (firstName == null || firstName.isEmpty()) {
             throw new IllegalArgumentException();
         }
         if (lastName == null || lastName.isEmpty()) {
             throw new IllegalArgumentException();
         }
-        if (employees.contains(employee)) {
+        if (employees.containsKey(key)) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
+        Employee employee = new Employee(firstName, lastName);
+        employees.put(key, employee);
         return employee;
     }
 
     public Employee removeEmployee(String firstName, String lastName) {
-        Employee target = new Employee(firstName, lastName);
         if (firstName == null || firstName.isEmpty()) {
             throw new IllegalArgumentException();
         }
         if (lastName == null || lastName.isEmpty()) {
             throw new IllegalArgumentException();
         }
-        if (!employees.remove(target)) {
+        Employee employee = employees.remove(getKey(firstName, lastName));
+        if (employee == null) {
             throw new EmployeeNotFoundException();
         }
-        return target;
+        return employee;
     }
 
     public Employee findEmployee(String firstName, String lastName) {
-        Employee target = new Employee(firstName, lastName);
         if (firstName == null || firstName.isEmpty()) {
             throw new IllegalArgumentException();
         }
         if (lastName == null || lastName.isEmpty()) {
             throw new IllegalArgumentException();
         }
-        int targetIndex = employees.indexOf(target);
-        if (targetIndex < 0) {
+        String key = getKey(firstName, lastName);
+        Employee employee = employees.get(key);
+        if (employee == null) {
             throw new EmployeeNotFoundException();
         }
-        return employees.get(targetIndex);
+        return employee;
     }
 
-    public List<Employee> getEmployees() {
-        return Collections.unmodifiableList(employees);
+    public Collection<Employee> getEmployees() {
+        return employees.values();
+    }
+
+    private String getKey(String firstName, String lastName) {
+        return firstName + lastName;
     }
 }

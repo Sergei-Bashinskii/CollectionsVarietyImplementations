3e7b1deaf8568e682cd78e858b659d3e0ad54793
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-03-24 18:13:33 +0300

    TaskMockito

--- src/main/java/com/example/Collections/sheets/and/sets/controller/DepartmentController.java
+++ src/main/java/com/example/Collections/sheets/and/sets/controller/DepartmentController.java
@@ -3,10 +3,7 @@ package com.example.Collections.sheets.and.sets.controller;
 import com.example.Collections.sheets.and.sets.model.Employee;
 import com.example.Collections.sheets.and.sets.service.DepartmentsService;
 import com.example.Collections.sheets.and.sets.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
@@ -21,22 +18,27 @@ public class DepartmentController {
         this.service = service;
     }
 
-    @GetMapping(path = "/max-salary")
-    public Employee maxSalaryDepartmentId(@RequestParam int departmentId) {
+    @GetMapping(path = "/{departmentId}/salary/max")
+    public Employee maxSalaryDepartmentId(@PathVariable int departmentId) {
         return service.maxSalaryDepartmentId(departmentId);
     }
 
-    @GetMapping(path = "/min-salary")
-    public Employee minSalaryDepartmentId(@RequestParam int departmentId) {
+    @GetMapping(path = "/{departmentId}/salary/min")
+    public Employee minSalaryDepartmentId(@PathVariable int departmentId) {
         return service.minSalaryDepartmentId(departmentId);
     }
 
-    @GetMapping(value = "/all", params = {"departmentId"})
-    public Collection<Employee> findByDepartment(@RequestParam int departmentId) {
+    @GetMapping(path = "/{departmentId}/employees")
+    public Collection<Employee> findByDepartment(@PathVariable int departmentId) {
         return service.findByDepartmentId(departmentId);
     }
 
-    @GetMapping(path = "/all")
+    @GetMapping(path = "/{departmentId}/salary/sum")
+    public int getSalaryByDepartment(@PathVariable int departmentId) {
+        return service.getSalarySumByDepartment(departmentId);
+    }
+
+    @GetMapping(path = "/employees")
     public Map<Integer, List<Employee>> groupByDepartment() {
         return service.groupByDepartmentId();
     }
